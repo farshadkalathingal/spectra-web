1b16ba1e28459c2b96b03337af87b1decd148542
Shad <farshadkalathingal@gmail.com>
2020-05-20 15:10:51 +0530

    loader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('lottie-player', false);
+});
 $(document).ready(function(){
 
     $("#gallery img").click(function(){
